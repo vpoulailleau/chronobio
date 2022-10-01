f26f1b6b4ef13d4ee331b607a5599016f1f30ae8
Vincent Poulailleau <vpoulailleau@gmail.com>
2022-10-01 19:06:34 +0200

    vegetable delivery

--- chronobio/game/soup_factory.py
+++ chronobio/game/soup_factory.py
@@ -1,12 +1,21 @@
-from chronobio.game.constants import DAYS_OFF_PER_FIRE, DAYS_OFF_PER_FLOOD
+from chronobio.game.constants import (
+    DAYS_OFF_PER_FIRE,
+    DAYS_OFF_PER_FLOOD,
+    VEGETABLE_PER_STOCK_DELIVERY,
+)
+from chronobio.game.vegetable import Vegetable
 
 
 class SoupFactory:
     def __init__(self):
         self.days_off = 0
+        self.stock: dict[Vegetable, int] = {vegetable: 0 for vegetable in Vegetable}
 
-    def flood(self):
+    def flood(self: "SoupFactory") -> None:
         self.days_off += DAYS_OFF_PER_FLOOD
 
-    def fire(self):
+    def fire(self: "SoupFactory") -> None:
         self.days_off += DAYS_OFF_PER_FIRE
+
+    def deliver(self: "SoupFactory", vegetable: Vegetable) -> None:
+        self.stock[vegetable] += VEGETABLE_PER_STOCK_DELIVERY
