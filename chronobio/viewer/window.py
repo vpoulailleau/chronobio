4d8fbc8f3b0b901a96dfd916c57bbff80b701b61
Vincent Poulailleau <vpoulailleau@gmail.com>
2022-10-22 16:52:49 +0200

    add grass

--- chronobio/viewer/window.py
+++ chronobio/viewer/window.py
@@ -1,8 +1,8 @@
 import arcade
 
 # Constants
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 650
+SCREEN_WIDTH = 1777
+SCREEN_HEIGHT = 1000
 SCREEN_TITLE = "Chronobio"
 
 
@@ -10,16 +10,24 @@ class Window(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
         arcade.set_background_color(arcade.csscolor.DARK_OLIVE_GREEN)
+        self.background_list: arcade.SpriteList = None
 
     def setup(self):
-        """Set up the game here. Call this function to restart the game."""
-        pass
+        self.background_list = arcade.SpriteList()
+        grass = arcade.Sprite("chronobio/viewer/images/grass.jpeg")
+        grass_width = grass.width
+        grass_height = grass.height
+        for x in range(0, SCREEN_WIDTH, grass_width):
+            for y in range(0, SCREEN_HEIGHT, grass_height):
+                grass = arcade.Sprite("chronobio/viewer/images/grass.jpeg")
+                grass.position = x + grass_width // 2, y + grass_height // 2
+                self.background_list.append(grass)
 
     def on_draw(self):
         self.clear()
+        self.background_list.draw()
 
 
 def gui_thread(window):
     window.setup()
     arcade.run()
-    
