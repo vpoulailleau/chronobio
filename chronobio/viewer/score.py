229a717e43d70111a7e9a95444207da3028ca6f1
Vincent Poulailleau <vpoulailleau@gmail.com>
2022-11-17 14:54:14 +0100

    begin text management

--- chronobio/viewer/score.py
+++ chronobio/viewer/score.py
@@ -1,28 +1,36 @@
 import arcade
 
+from chronobio.game.constants import MAX_NB_PLAYERS
 from chronobio.viewer.constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+MARGIN = 20
+WIDTH = SCREEN_WIDTH / 3 - 2 * MARGIN
+HEIGHT = SCREEN_HEIGHT - 2 * MARGIN
+CENTER_X = SCREEN_WIDTH / 6
+CENTER_Y = SCREEN_HEIGHT / 2
 
 
 class Score:
     def __init__(self):
-        self.sprite_list = arcade.SpriteList()
-
-        # soup_factory = arcade.Sprite("chronobio/viewer/images/factory_small.png")
-        # soup_factory.position = self.rotate(SOUP_FACTORY_DISTANCE_FROM_CENTER, 0)
-        # soup_factory.width = SOUP_FACTORY_WIDTH
-        # soup_factory.height = SOUP_FACTORY_WIDTH
-        # soup_factory.angle = angle
-        # self.sprite_list.append(soup_factory)
+        self.state = None
 
     def update(self, game_state: dict) -> None:
-        self.sprite_list.clear()
+        self.state = game_state
 
     def draw(self) -> None:
-        # arcade.draw_text("draw_filled_rect", 363, 3, arcade.color.BLACK, 10)
         arcade.draw_rectangle_filled(
-            center_x=SCREEN_WIDTH / 6,
-            center_y=SCREEN_HEIGHT / 2,
-            width=SCREEN_WIDTH / 3 - 40,
-            height=SCREEN_HEIGHT - 40,
+            center_x=CENTER_X,
+            center_y=CENTER_Y,
+            width=WIDTH,
+            height=HEIGHT,
             color=(255, 255, 255, 100),
         )
+        for n in range(MAX_NB_PLAYERS):
+            arcade.draw_text(
+                "Nom",
+                start_x=MARGIN * 2,
+                start_y=MARGIN * 2 + (HEIGHT - 2 * MARGIN) / (MAX_NB_PLAYERS) * n,
+                color=arcade.color.BROWN_NOSE,
+                font_size=20,
+                font_name="Kenney Blocks",
+            )
