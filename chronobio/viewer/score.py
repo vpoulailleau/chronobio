7aacb2401fd0d1e6f7e0735e2d87242cffcf34a8
Vincent Poulailleau <vpoulailleau@gmail.com>
2022-11-17 16:45:57 +0100

    display player name

--- chronobio/viewer/score.py
+++ chronobio/viewer/score.py
@@ -8,11 +8,12 @@ WIDTH = SCREEN_WIDTH / 3 - 2 * MARGIN
 HEIGHT = SCREEN_HEIGHT - 2 * MARGIN
 CENTER_X = SCREEN_WIDTH / 6
 CENTER_Y = SCREEN_HEIGHT / 2
+NAME_OFFSET = 100
 
 
 class Score:
     def __init__(self):
-        self.state = None
+        self.state: dict = {}
 
     def update(self, game_state: dict) -> None:
         self.state = game_state
@@ -25,11 +26,15 @@ class Score:
             height=HEIGHT,
             color=(255, 255, 255, 100),
         )
+        if "farms" not in self.state:
+            return
         for n in range(MAX_NB_PLAYERS):
             arcade.draw_text(
-                "Nom",
+                self.state["farms"][n]["name"],
                 start_x=MARGIN * 2,
-                start_y=MARGIN * 2 + (HEIGHT - 2 * MARGIN) / (MAX_NB_PLAYERS) * n,
+                start_y=NAME_OFFSET
+                + MARGIN * 2
+                + (HEIGHT - 2 * MARGIN) / (MAX_NB_PLAYERS) * n,
                 color=arcade.color.BROWN_NOSE,
                 font_size=20,
                 font_name="Kenney Blocks",
