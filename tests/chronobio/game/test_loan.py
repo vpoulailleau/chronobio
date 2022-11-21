f552eebbc6727b57820b046c9a92e02af2f497bd
Vincent Poulailleau <vpoulailleau@gmail.com>
2022-11-21 19:04:24 +0100

    test month cost

--- tests/chronobio/game/test_loan.py
+++ tests/chronobio/game/test_loan.py
@@ -1,7 +1,24 @@
+from chronobio.game.constants import LOAN_DURATION_IN_MONTHS, LOAN_INTEREST
 from chronobio.game.loan import Loan
+
+
+def test_month_cost_before_loan():
+    loan = Loan(amount=100, start_day=10)
+    assert loan.month_cost(day=5) == 0
+
+
+def test_month_cost_after_loan():
+    loan = Loan(amount=100, start_day=10)
+    assert loan.month_cost(day=10 + LOAN_DURATION_IN_MONTHS * 30) == 0
+
+
+def test_month_cost_during_loan():
+    loan = Loan(amount=100, start_day=10)
+    assert loan.month_cost(day=20) == int(
+        loan.amount * LOAN_INTEREST / LOAN_DURATION_IN_MONTHS
+    )
 
 
 def test_remaining_amount_at_start():
     loan = Loan(amount=100, start_day=10)
-
     assert loan.amount == 100
