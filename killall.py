3d3da60271daad707fed9b8d5baf58e79cca70cf
Vincent Poulailleau <vpoulailleau@gmail.com>
2022-11-22 14:12:10 +0100

    more general detection

--- killall.py
+++ killall.py
@@ -6,7 +6,7 @@ stdout = result.stdout.decode("utf-8")
 for line in stdout.splitlines():
     if "python" not in line:
         continue
-    if "chronobio." in line or "sample_player_client" in line:
+    if "chronobio." in line or ("sample_" in line and "_player" in line):
         print(line)
         pid = line.split()[1]
         subprocess.run(["kill", "-9", pid])
