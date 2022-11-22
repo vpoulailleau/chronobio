3b546414124b8697003fa9dc8d83f6f466bc1186
Vincent Poulailleau <vpoulailleau@gmail.com>
2022-11-22 15:55:31 +0100

    start competition

---
+++ competition.sh
@@ -0,0 +1,24 @@
+PORT=$(python -c "import random; print(random.randint(2000, 3000))")
+
+python killall.py
+
+for team in `ls teams`
+do
+    echo "#################"
+    echo "Updating $team"
+    cd teams/$team
+    git pull
+    cd -
+done
+
+python -m chronobio.game.server -p $PORT &
+python -m chronobio.viewer -p $PORT &
+
+for team in `ls teams`
+do
+    echo "#################"
+    echo "Starting $team"
+    cd teams/$team
+    ./_launch.sh $PORT &
+    cd -
+done
