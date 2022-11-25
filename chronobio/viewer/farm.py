9a6229975a6645cedf966af89f61a7b0a30663fa
Vincent Poulailleau <vpoulailleau@gmail.com>
2022-11-25 09:57:53 +0100

    avoid sprite list creation at each frame

--- chronobio/viewer/farm.py
+++ chronobio/viewer/farm.py
@@ -102,6 +102,7 @@ class Farm:
         self.vegetables: list[Vegetable] = []
         self.soups: list[Soup] = []
         self.soup_angle = 0
+        self.sprite_list = arcade.SpriteList()
 
     def rotate(self, x, y):
         cos = math.cos(math.radians(self.angle))
@@ -113,33 +114,40 @@ class Farm:
         seen = set()
         for employee in data["employees"]:
             seen.add(employee["id"])
-            employee_entity = self.employees.get(
-                employee["id"],
-                MovingEntity("chronobio/viewer/images/farmer.png"),
-            )
-            employee_entity.sprite.width = 60
-            employee_entity.sprite.height = 60
-            employee_entity.target_location = Location[employee["location"]]
-            self.employees[employee["id"]] = employee_entity
+            if employee["id"] not in self.employees:
+                employee_entity = MovingEntity("chronobio/viewer/images/farmer.png")
+                employee_entity.sprite.width = 60
+                employee_entity.sprite.height = 60
+                self.sprite_list.append(employee_entity.sprite)
+                self.employees[employee["id"]] = employee_entity
+            self.employees[employee["id"]].target_location = Location[
+                employee["location"]
+            ]
         for employee_id in list(self.employees):
             if employee_id not in seen:
-                del self.employees[employee_id]
+                employee = self.employees.pop(employee_id)
+                self.sprite_list.remove(employee.sprite)
 
         seen = set()
         for tractor in data["tractors"]:
             seen.add(tractor["id"])
-            tractor_entity = self.tractors.get(
-                tractor["id"],
-                MovingEntity("chronobio/viewer/images/tractor.png"),
-            )
-            tractor_entity.sprite.width = 60
-            tractor_entity.sprite.height = 60
-            tractor_entity.target_location = Location[tractor["location"]]
-            self.tractors[tractor["id"]] = tractor_entity
+            if tractor["id"] not in self.tractors:
+                tractor_entity = MovingEntity("chronobio/viewer/images/tractor.png")
+                tractor_entity.sprite.width = 60
+                tractor_entity.sprite.height = 60
+                self.sprite_list.append(tractor_entity.sprite)
+                self.tractors[tractor["id"]] = tractor_entity
+            self.tractors[tractor["id"]].target_location = Location[tractor["location"]]
         for tractor_id in list(self.tractors):
             if tractor_id not in seen:
-                del self.tractors[tractor_id]
+                tractor = self.tractors.pop(tractor_id)
+                self.sprite_list.remove(tractor.sprite)
 
+        for vegetable in self.vegetables:
+            try:
+                self.sprite_list.remove(vegetable.sprite)
+            except (KeyError, ValueError):
+                pass  # empty field
         self.vegetables.clear()
         for field in data["fields"]:
             vegetable = Vegetable(
@@ -148,36 +156,31 @@ class Farm:
             vegetable.location(Location[field["location"]], farm=self)
             vegetable.update_size(field["needed_water"])
             self.vegetables.append(vegetable)
+            if "/transparent.png" not in vegetable.sprite_path:
+                self.sprite_list.append(vegetable.sprite)
 
         for event in data["events"]:
             if "[SOUP]" in event:
                 nb_vegetables = int(event.split()[1])
-                self.soups.append(
-                    Soup(angle=self.soup_angle, nb_vegetables=nb_vegetables)
-                )
+                soup = Soup(angle=self.soup_angle, nb_vegetables=nb_vegetables)
+                self.soups.append(soup)
                 self.soup_angle += 10
+                self.sprite_list.append(soup.sprite)
 
     def draw(self):
-        sprite_list = arcade.SpriteList()
 
         for tractor in self.tractors.values():
             tractor.update_position(self)
-            sprite_list.append(tractor.sprite)
 
         for employee in self.employees.values():
             employee.update_position(self)
-            sprite_list.append(employee.sprite)
-
-        for vegetable in self.vegetables:
-            if "/transparent.png" not in vegetable.sprite_path:
-                sprite_list.append(vegetable.sprite)
 
         for soup in self.soups:
             soup.update_position(self)
-            sprite_list.append(soup.sprite)
 
-        sprite_list.draw()
+        self.sprite_list.draw()
 
         for soup in self.soups.copy():
             if soup.radius / soup.MAX_RADIUS > 0.9:
                 self.soups.remove(soup)
+                self.sprite_list.remove(soup.sprite)
