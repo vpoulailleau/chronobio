b3d409ea5a3d8c7d647e8cd38310f21577a9d314
Vincent Poulailleau <vpoulailleau@gmail.com>
2023-01-12 16:27:19 +0100

    possibility to skip git update

--- competition.sh
+++ competition.sh
@@ -1,16 +1,21 @@
+SKIP_UPDATE=$1
+
 PORT=$(python -c "import random; print(random.randint(2000, 3000))")
 
 python killall.py
 rm *.log
 
-for team in `ls teams`
-do
-    echo "#################"
-    echo "Updating $team"
-    cd teams/$team
-    git pull
-    cd -
-done
+if [ -z "$SKIP_UPDATE" ]
+then
+    for team in `ls teams`
+    do
+        echo "#################"
+        echo "Updating $team"
+        cd teams/$team
+        git pull
+        cd -
+    done
+fi
 
 python -m chronobio.game.server -p $PORT &
 python -m chronobio.viewer -p $PORT &
