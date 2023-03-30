fa8c22a137bf6c52fa60c490a9608ac18c60d5d5
Vincent Poulailleau <vpoulailleau@gmail.com>
2023-03-30 11:19:31 +0200

    apply linter suggestions

--- chronobio/viewer/viewer.py
+++ chronobio/viewer/viewer.py
@@ -12,9 +12,7 @@ class Viewer(Client):
     def __init__(self: "Viewer", server_addr: str, port: int) -> None:
         super().__init__(server_addr, port, spectator=True)
         self.window = Window()
-        self.gui_thread = Thread(
-            target=gui_thread, args=(self.window,), daemon=True
-        ).start()
+        Thread(target=gui_thread, args=(self.window,), daemon=True).start()
 
     def run(self: "Viewer") -> NoReturn:
         while True:
