af604c5b2a3314056bb53dba38d7c6490b4c1a2a
Vincent Poulailleau <vpoulailleau@gmail.com>
2024-10-29 16:05:48 +0100

    use resources

--- chronobio/viewer/farm_background.py
+++ chronobio/viewer/farm_background.py
@@ -1,4 +1,5 @@
 import math
+from importlib.resources import files
 
 import arcade
 
@@ -30,7 +31,9 @@ class FarmBackround:
             color=COLORS[self.player],
         )
 
-        soup_factory = arcade.Sprite("chronobio/viewer/images/factory_small.png")
+        soup_factory = arcade.Sprite(
+            files("chronobio.viewer").joinpath("images/factory_small.png")
+        )
         soup_factory.position = self.rotate(SOUP_FACTORY_DISTANCE_FROM_CENTER, 0)
         soup_factory.width = SOUP_FACTORY_WIDTH
         soup_factory.height = SOUP_FACTORY_WIDTH
@@ -38,14 +41,16 @@ class FarmBackround:
         self.sprite_list.append(soup_factory)
 
         for field_index in range(5):
-            field = arcade.Sprite("chronobio/viewer/images/field.jpg")
+            field = arcade.Sprite(
+                files("chronobio.viewer").joinpath("images/field.jpg")
+            )
             field.position = self.rotate(FIELD_OFFSET + field_index * FIELD_DISTANCE, 0)
             field.width = FIELD_WIDTH
             field.height = 2 * FIELD_WIDTH
             field.angle = angle
             self.sprite_list.append(field)
 
-        farm = arcade.Sprite("chronobio/viewer/images/farm.png")
+        farm = arcade.Sprite(files("chronobio.viewer").joinpath("images/farm.png"))
         farm.position = self.rotate(FARM_BUILDING_DISTANCE_FROM_CENTER, 0)
         farm.width = FARM_BUILDING_WIDTH
         farm.height = FARM_BUILDING_WIDTH
